33ef20d0b00fe80d40f3adc7669651bec1831808
Hongzhi Li <hongzhi.li@senior798.eu>
2022-04-20 06:34:40 +0200

    added app.js to acheive the function of visiters counted

---
+++ app.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  getVisitCount();
+});
+
+const functionApiUrl =
+  "https://api.countapi.xyz/hit/bluehawana.com/visits?callback=liveViews";
+const localFunctionApi = "http://localhost:7071/api/GetResumeCounter";
+
+const getVisitCount = () => {
+  let count = 1;
+  fetch(functionApiUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log("Website called function API.");
+      count = response.count;
+      document.getElementById("visiters").innerText = count;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return count;
+};
