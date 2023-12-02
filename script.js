1846c8cf71eacd0908bf3f023ae1bfcfb3a2a00a
Hongzhi Li <bluehawana@gmail.com>
2023-12-02 18:13:40 +0100

    try to fix the hover issues

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
 // script.js
 
-function turnstileCallback(token) {
-  console.log("Turnstile token:", token);
-  // You can perform actions here after the Turnstile challenge is completed
-  // Example: Hide the Turnstile widget
-  var turnstileWidget = document.querySelector(".cf-turnstile");
-  if (turnstileWidget) {
-    turnstileWidget.style.display = "none";
-  }
+function turnstileCallback(response) {
+  console.log("Turnstile verification response:", response);
 
-  // If you had a server-side endpoint, you'd send the token to it for validation
-  // Example:
-  // fetch('your_serverless_function_endpoint', {
-  //     method: 'POST',
-  //     headers: {
-  //         'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ token: token }),
-  // })
-  // .then(response => response.json())
-  // .then(data => {
-  //     console.log('Validation response:', data);
-  //     // Handle the response here
-  // })
-  // .catch(error => {
-  //     console.error('Error:', error);
-  // });
+  // Delay execution to ensure Turnstile is fully loaded and interactive
+  setTimeout(function () {
+    var turnstileOverlay = document.querySelector(".cf-turnstile");
+    if (turnstileOverlay) {
+      console.log("Hiding Turnstile overlay");
+      turnstileOverlay.style.display = "none";
+    } else {
+      console.error("Turnstile overlay not found");
+    }
+  }, 500); // Delay of 500 milliseconds
 }
