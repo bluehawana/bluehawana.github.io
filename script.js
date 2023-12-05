ba8d7f63f783776a7d4acca49a4bb73d6de847f1
Hongzhi Li <bluehawana@gmail.com>
2023-12-05 13:33:17 +0100

    fixing script.js index.html  and cloudflare worker.js and test it again

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // script.js
 
-function turnstileCallback(token) {
+// Function to send the Turnstile token to your Cloudflare Worker for verification
+function sendTokenToServer(token) {
   console.log("Turnstile token:", token);
   const workerEndpoint = "https://turnstilebot.bluehawana.workers.dev/";
 
@@ -9,21 +10,43 @@ function turnstileCallback(token) {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ token: token }),
   })
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
     .then((data) => {
       console.log("Verification result:", data);
       if (data.success) {
         console.log("CAPTCHA verification successful");
-        // Continue with form submission or other actions
+        // Reveal the rest of the website or continue with form submission
+        document.getElementById("your-website-content").style.display = "block";
       } else {
         console.error(
           "CAPTCHA verification failed",
           data["error-codes"],
           data.messages
         );
+        // Handle CAPTCHA verification failure
       }
     })
     .catch((error) => {
       console.error("Error during CAPTCHA verification:", error);
     });
 }
+
+// Function to render the Turnstile widget and handle the generated token
+function onloadTurnstileCallback() {
+  turnstile.render("#example-container", {
+    sitekey: "0x4AAAAAAALTkPrvFDCTqdiq", // Replace with your actual Turnstile site key
+    callback: function (token) {
+      console.log(`Challenge Success: ${token}`);
+      // Send the token to your server-side for verification
+      sendTokenToServer(token);
+    },
+  });
+}
+
+// Ensure the Turnstile callback function is loaded after the window loads
+window.onload = onloadTurnstileCallback;
