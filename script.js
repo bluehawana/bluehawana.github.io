ff8bda00346c5cc1529964afbc21974b38eee3fa
Hongzhi Li <bluehawana@gmail.com>
2023-12-05 12:06:40 +0100

    fixing script.js and cloudflare worker

--- script.js
+++ script.js
@@ -2,27 +2,25 @@
 
 function turnstileCallback(token) {
   console.log("Turnstile token:", token);
-
-  // Replace with your Cloudflare Worker's endpoint
   const workerEndpoint = "https://turnstilebot.bluehawana.workers.dev/";
 
   fetch(workerEndpoint, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ token: token }),
   })
     .then((response) => response.json())
     .then((data) => {
       console.log("Verification result:", data);
       if (data.success) {
-        // CAPTCHA verified successfully
-        // You can now proceed with form submission or other actions
         console.log("CAPTCHA verification successful");
+        // Continue with form submission or other actions
       } else {
-        // CAPTCHA verification failed
-        console.error("CAPTCHA verification failed");
+        console.error(
+          "CAPTCHA verification failed",
+          data["error-codes"],
+          data.messages
+        );
       }
     })
     .catch((error) => {
